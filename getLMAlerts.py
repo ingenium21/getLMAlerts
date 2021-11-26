d1c99ad0a7ea926e9f045effa0e948ed6952d57c
Renato Regalado <renato.regalado@gmail.com>
2021-11-26 13:40:35 -0600

    finished the alert getting portion

--- getLMAlerts.py
+++ getLMAlerts.py
@@ -7,6 +7,8 @@ from pprint import pprint
 import json
 import re
 from dotenv import load_dotenv
+import os
+
 
 load_dotenv()
 
@@ -14,14 +16,19 @@ def get_alerts():
     """gets the alerts and returns an array of them """
     alerts = api_instance.get_alert_list(size=3000)
     alerts = alerts.to_dict()
-    alerts = alerts["items"]
     return alerts
+
+def alerts_to_json(alerts):
+    """spits out alerts to a json file specified in a directory in .env"""
+    filepath = os.getenv("JSON_PATH")
+    with open(filepath, 'w') as fp:
+        json.dump(alerts, fp)
 
 # Configure API key authorization: LMv1
 configuration = logicmonitor_sdk.Configuration()
-configuration.company = COMPANY
-configuration.access_id = ACCESS_ID
-configuration.access_key = ACCESS_KEY
+configuration.company = os.getenv("COMPANY")
+configuration.access_id = os.getenv("ACCESS_ID")
+configuration.access_key = os.getenv("ACCESS_KEY")
 
 # create an instance of the API class
 api_instance = logicmonitor_sdk.LMApi(logicmonitor_sdk.ApiClient(configuration))
@@ -29,4 +36,4 @@ api_instance = logicmonitor_sdk.LMApi(logicmonitor_sdk.ApiClient(configuration))
 
 if __name__ == "__main__":
     alarms = get_alerts()
-    pprint(alarms[0])
+    alerts_to_json(alarms)
